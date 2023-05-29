1872aa0ad823ac943d712142ff4c993e42bc0dc8
Tomasz Ślanda <ts@enkod.pl>
2023-05-29 19:45:21 +0200

    tests(streaming): implemented native websocket class, fixed tests

---
+++ Assets/Charactr/VoiceSDK/Tests/NativeSocketWrapper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Charactr.VoiceSDK.Tests
+{
+	public class NativeSocketWrapper
+	{
+		public WebSocketState Status => _ws.State;
+		public event Action OnOpen;
+		public event Action<string> OnClose;
+		public event Action<byte[]> OnData;
+		public event Action<string> OnError;
+		
+		private readonly ClientWebSocket _ws;
+		private readonly CancellationTokenSource _token;
+		private readonly Uri _uri;
+		private Task _dispatch;
+		public NativeSocketWrapper(string url, int timeout = 5000)
+		{
+			_ws = new ClientWebSocket();
+			_uri = new Uri(url);
+			_token = new CancellationTokenSource();
+			_token.CancelAfter(timeout);
+		}
+
+		public async void Connect()
+		{
+			await _ws.ConnectAsync(_uri, _token.Token);
+			
+			if (Status == WebSocketState.Open)
+				OnOpen?.Invoke();
+			else
+				OnError?.Invoke(Status.ToString());
+
+			_dispatch = Dispatch();
+		}
+
+		public async void SendText(string text)
+		{
+			var bytesToSendConvert = new ArraySegment<byte>(Encoding.UTF8.GetBytes(text));
+			await _ws.SendAsync(bytesToSendConvert, WebSocketMessageType.Text, true, _token.Token);
+		}
+
+		public async void Close()
+		{
+			if (Status != WebSocketState.Open)
+				throw new Exception("Can't close, status: " + Status);
+
+			_dispatch.Dispose();
+			var description = "Closing on user request";
+			await _ws.CloseAsync(WebSocketCloseStatus.NormalClosure, description , _token.Token);
+			OnClose?.Invoke(description);
+		}
+		
+		private async Task Dispatch()
+		{
+			var rcvBytes = new byte[128];
+			var rcvBuffer = new ArraySegment<byte>(rcvBytes);
+			WebSocketReceiveResult result = null;
+			
+			do
+			{
+				result = await _ws.ReceiveAsync(rcvBuffer, _token.Token);
+				var msgBytes = rcvBuffer.Skip(rcvBuffer.Offset).Take(result.Count).ToArray();
+				OnData?.Invoke(msgBytes);
+			} 
+			while (result.CloseStatus == null);
+			
+			if (result.CloseStatus == WebSocketCloseStatus.NormalClosure)
+			{
+				OnClose?.Invoke(result.CloseStatusDescription);
+			}
+			else
+			{
+				OnError?.Invoke(result.CloseStatusDescription);
+			}
+		}
+	}
+}
--- Assets/Charactr/VoiceSDK/Tests/Streaming.cs
+++ Assets/Charactr/VoiceSDK/Tests/Streaming.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 using Charactr.VoiceSDK.Streaming;
 using Newtonsoft.Json;
 using NUnit.Framework;
+using UnityEditor.Experimental;
 using UnityEditor.PackageManager;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using Task = System.Threading.Tasks.Task;
 
 namespace Charactr.VoiceSDK.Tests
 {
-	public class Streaming
+	public partial class Streaming
 	{
 		private const string Text = "Hello from Charactr Software Development Kit for Unity";
+		private const int VoiceId = 112;
 		private async Task<ClientWebSocket> GetClient(int voiceId = 151)
 		{
 			var ws = new ClientWebSocket();
@@ -23,6 +27,18 @@ namespace Charactr.VoiceSDK.Tests
 
 			await ws.ConnectAsync(serverUri, source.Token);
 			return ws;
+		}
+
+		private Configuration _configuration;
+		
+		[SetUp]
+		public void Setup()
+		{
+			_configuration = Configuration.LoadStreaming();
+			
+			Assert.NotNull(_configuration);
+			Assert.IsNotEmpty(_configuration.ApiClient);
+			Assert.IsNotEmpty(_configuration.ApiKey);
 		}
 		
 		[Test]
@@ -37,17 +53,12 @@ namespace Charactr.VoiceSDK.Tests
 		[Test]
 		public async Task SendAuthMessage_StatusPolicyViolation()
 		{
-			var config = Configuration.LoadStreaming();
-			Assert.NotNull(config);
-			Assert.IsNotEmpty(config.ApiClient);
-			Assert.IsNotEmpty(config.ApiKey);
-			
 			var ws = await GetClient(151);
 			
 			Assert.NotNull(ws);
 			Assert.IsTrue(ws.State == WebSocketState.Open);
 			
-			var authCommand = AudioStreamingClientBase.GetAuthCommand(config.ApiKey, config.ApiClient);
+			var authCommand = AudioStreamingClientBase.GetAuthCommand(_configuration.ApiKey, _configuration.ApiClient);
 			
 			var bytesToSendAuth = new ArraySegment<byte>(Encoding.UTF8.GetBytes(authCommand));
 			await ws.SendAsync(bytesToSendAuth, WebSocketMessageType.Text, true, CancellationToken.None);
@@ -65,21 +76,16 @@ namespace Charactr.VoiceSDK.Tests
 			Assert.IsTrue(result.EndOfMessage);
 			Assert.IsTrue(result.CloseStatus == WebSocketCloseStatus.PolicyViolation);
 		}
-		
+
 		[Test]
 		public async Task SendConvertMessage_StatusNormalClosure()
 		{
-			var config = Configuration.LoadStreaming();
-			Assert.NotNull(config);
-			Assert.IsNotEmpty(config.ApiClient);
-			Assert.IsNotEmpty(config.ApiKey);
-			
-			var ws = await GetClient(112);
+			var ws = await GetClient(VoiceId);
 			
 			Assert.NotNull(ws);
 			Assert.IsTrue(ws.State == WebSocketState.Open);
 			
-			var authCommand = AudioStreamingClientBase.GetAuthCommand(config.ApiKey, config.ApiClient);
+			var authCommand = AudioStreamingClientBase.GetAuthCommand(_configuration.ApiKey, _configuration.ApiClient);
 			
 			var bytesToSendAuth = new ArraySegment<byte>(Encoding.UTF8.GetBytes(authCommand));
 			await ws.SendAsync(bytesToSendAuth, WebSocketMessageType.Text, true, CancellationToken.None);
@@ -106,8 +112,50 @@ namespace Charactr.VoiceSDK.Tests
 			Assert.GreaterOrEqual(total,1);
 			Assert.IsTrue(result.EndOfMessage);
 			Assert.IsTrue(result.CloseStatus == WebSocketCloseStatus.NormalClosure);
+			Assert.AreEqual(219180,total);
+			
 			Debug.Log("Total:" + total);
 			Debug.Log("Total time:" + total / 2f / 44100f);
+		}
+
+		[Test]
+		public async Task SendConvertMessage_Wrapper_StatusNormalClosure()
+		{
+			var bytesCount = 0;
+			var closeStatus = string.Empty;
+			var authCommand = AudioStreamingClientBase.GetAuthCommand(_configuration.ApiKey, _configuration.ApiClient);
+			var convertCommand = AudioStreamingClientBase.GetConvertCommand(Text);
+			
+			var w = new NativeSocketWrapper(AudioStreamingManager.URL + $"?voiceId={VoiceId}");
+			
+			w.OnData += bytes =>
+			{
+				bytesCount += bytes.Length;
+				Debug.Log($"OnData: {bytes.Length}/{bytesCount}");
+			};
+			
+			w.OnOpen += () =>
+			{
+				w.SendText(authCommand);
+				w.SendText(convertCommand);
+			};
+			
+			w.OnClose += s =>
+			{
+				closeStatus = s;
+				Debug.Log("Close: "+ closeStatus);
+			};
+			
+			w.OnError += s =>
+			{
+				closeStatus = s;
+				Debug.LogError("Error: " + closeStatus);
+			};
+			
+			w.Connect();
+			await Task.Delay(3000);
+			Assert.IsTrue(w.Status == WebSocketState.CloseReceived);
+			Assert.AreEqual(219180, bytesCount);
 		}
 	}
 }
